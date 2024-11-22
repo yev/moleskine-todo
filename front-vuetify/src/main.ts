69c0d4cfed274c7ba708d6fa7d067a4868f69a45
Voronetskyy Yevgen <yev@devatlant.com>
2024-11-22 09:08:10 +0100

    integrate APM for VUE.js

--- front-vuetify/src/main.ts
+++ front-vuetify/src/main.ts
@@ -13,7 +13,14 @@ import App from './App.vue'
 // Composables
 import { createApp } from 'vue'
 
-const app = createApp(App)
+import { ApmVuePlugin } from '@elastic/apm-rum-vue'
+
+const app = createApp(App).use(ApmVuePlugin, {
+  config: {
+    serviceName: 'moleskine-front-web',
+    // agent configuration
+  }
+})
 
 registerPlugins(app)
 
