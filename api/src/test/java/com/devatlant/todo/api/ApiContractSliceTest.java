0de6647bad1ffc968b479b7b6e9e5de1c3d9ae5b
Voronetskyy Yevgen <yev@devatlant.com>
2024-11-26 10:11:59 +0100

    use MockitoBean instead of deprecated @MockBean

--- api/src/test/java/com/devatlant/todo/api/ApiContractSliceTest.java
+++ api/src/test/java/com/devatlant/todo/api/ApiContractSliceTest.java
@@ -12,8 +12,8 @@ import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.GitProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import static org.mockito.Mockito.verify;
@@ -31,7 +31,7 @@ public class ApiContractSliceTest {
     @Autowired
     private MockMvc mvc;
 
-    @MockBean
+    @MockitoBean
     private TodoService service;
 
     @Captor ArgumentCaptor<Todo> captor;
